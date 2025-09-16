e8cd0cca3fafe6019e69a1bf61109a410edb4bda
Niko H. <Sehrentos@users.noreply.github.com>
2025-09-16 08:42:39 +0300

    fix API login generateToken expires option

--- server/src/api/login/index.js
+++ server/src/api/login/index.js
@@ -6,117 +6,123 @@ const loginRouter = express.Router({ caseSensitive: false });
 
 // route /api/login
 loginRouter.post('/', async (req, res, next) => {
-    const { username, password } = req.body;
-    let last_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    last_ip = Array.isArray(last_ip) ? last_ip[0] : last_ip;
+	const { username, password } = req.body;
+	let last_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+	last_ip = Array.isArray(last_ip) ? last_ip[0] : last_ip;
 
-    // check if username and password are provided
-    if (!username || !password) {
-        res.status(401);
-        res.json({ type: 'error', message: 'Unauthorized' });
-        return;
-    }
+	// check if username and password are provided
+	if (!username || !password) {
+		res.status(401);
+		res.json({ type: 'error', message: 'Unauthorized' });
+		return;
+	}
 
-    // load user data from database
-    let conn;
-    try {
-        conn = await DB.connect()
+	// load user data from database
+	let conn;
+	try {
+		conn = await DB.connect()
 
-        const account = await DB.account.login(username, password, last_ip);
+		const account = await DB.account.login(username, password, last_ip);
 
-        // generate JWT token, but filter some account props
-        const token = generateToken({
-            id: account.id, // this should be enough to identify the user later
-            email: account.email,
-            state: account.state,
-            expires: account.expires,
-            logincount: account.logincount,
-            lastlogin: account.lastlogin,
-            last_ip: account.last_ip,
-        }, process.env.JWT_EXPIRES || "2d")
+		// generate JWT token, but filter some account props
+		const token = generateToken({
+			id: account.id, // this should be enough to identify the user later
+			email: account.email,
+			state: account.state,
+			expires: account.expires,
+			logincount: account.logincount,
+			lastlogin: account.lastlogin,
+			last_ip: account.last_ip,
+		}, {
+			//@ts-ignore JWT_EXPIRES might be undefined or wrong format
+			expiresIn: process.env.JWT_EXPIRES || "2d"
+		})
 
-        // update web token in database
-        await DB.account.updateToken(account.id, token);
-        console.log('[API/login] account login:', account.id)
+		// update web token in database
+		await DB.account.updateToken(account.id, token);
+		console.log('[API/login] account login:', account.id)
 
-        res.json({
-            type: 'success',
-            message: 'JWT token',
-            token
-        });
-    } catch (err) {
-        console.log('[API/login] Error', err.message, err.code || '')
-        res.status(401);
-        res.json({ type: 'error', message: 'Invalid credentials' });
-    } finally {
-        if (conn) conn.end();
-    }
+		res.json({
+			type: 'success',
+			message: 'JWT token',
+			token
+		});
+	} catch (err) {
+		console.log('[API/login] Error', err.message, err.code || '')
+		res.status(401);
+		res.json({ type: 'error', message: 'Invalid credentials' });
+	} finally {
+		if (conn) conn.end();
+	}
 });
 
 // route /api/login/token
 loginRouter.post('/token', async (req, res, next) => {
-    const { token } = req.body;
+	const { token } = req.body;
 
-    // check if token exists
-    if (!token) {
-        res.status(401);
-        res.json({ type: 'error', message: 'Unauthorized' });
-        return;
-    }
+	// check if token exists
+	if (!token) {
+		res.status(401);
+		res.json({ type: 'error', message: 'Unauthorized' });
+		return;
+	}
 
 
-    try {
-        // verify/refresh token
-        await verifyToken(token);
-        // const payload = await verifyToken(token);
-        // console.log(`[API/login/token] is verified`, payload);
-    } catch (err) {
-        if (err instanceof jwt.TokenExpiredError) {
-            res.status(401);
-            res.json({ type: 'error', message: `Token expired: ${err.expiredAt}` });
-            return
-        }
-        console.log('[API/login/token] verify error:', err.message, err.code || '')
-        res.status(401);
-        res.json({ type: 'error', message: 'Token unverified' });
-        return;
-    }
+	try {
+		// verify/refresh token
+		await verifyToken(token);
+		// const payload = await verifyToken(token);
+		// console.log(`[API/login/token] is verified`, payload);
+	} catch (err) {
+		if (err instanceof jwt.TokenExpiredError) {
+			res.status(401);
+			res.json({ type: 'error', message: `Token expired: ${err.expiredAt}` });
+			return
+		}
+		console.log('[API/login/token] verify error:', err.message, err.code || '')
+		res.status(401);
+		res.json({ type: 'error', message: 'Token unverified' });
+		return;
+	}
 
-    // load user data from database
-    let conn;
-    try {
-        conn = await DB.connect()
+	// load user data from database
+	let conn;
+	try {
+		conn = await DB.connect()
 
-        // get account data with old token
-        const account = await DB.account.loginToken(token);
+		// get account data with old token
+		const account = await DB.account.loginToken(token);
 
-        // generate JWT token, but filter some account props
-        const jwtToken = generateToken({
-            id: account.id, // this should be enough to identify the user later
-            email: account.email,
-            state: account.state,
-            expires: account.expires,
-            logincount: account.logincount,
-            lastlogin: account.lastlogin,
-            last_ip: account.last_ip,
-        }, process.env.JWT_EXPIRES || "2d")
+		// generate JWT token, but filter some account props
+		const jwtToken = generateToken({
+			id: account.id, // this should be enough to identify the user later
+			email: account.email,
+			state: account.state,
+			expires: account.expires,
+			logincount: account.logincount,
+			lastlogin: account.lastlogin,
+			last_ip: account.last_ip,
+		}, {
+			//@ts-ignore JWT_EXPIRES might be undefined or wrong format
+			expiresIn: process.env.JWT_EXPIRES || "2d"
+		})
 
-        // update new web token in database
-        await DB.account.updateToken(account.id, jwtToken);
-        console.log('[API/login/token] account login:', account.id)
+		// update new web token in database
+		await DB.account.updateToken(account.id, jwtToken);
+		console.log('[API/login/token] account login:', account.id)
 
-        res.json({
-            type: 'success',
-            message: 'JWT token',
-            token: jwtToken
-        });
-    } catch (err) {
-        console.log('[API/login/token] Error', err.message, err.code || '')
-        res.status(401);
-        res.json({ type: 'error', message: 'Invalid token' });
-    } finally {
-        if (conn) conn.end();
-    }
+		res.json({
+			type: 'success',
+			message: 'JWT token',
+			token: jwtToken
+		});
+	} catch (err) {
+		console.log('[API/login/token] Error', err.message, err.code || '')
+		res.status(401);
+		res.json({ type: 'error', message: 'Invalid token' });
+	} finally {
+		if (conn) conn.end();
+	}
 });
 
 export default loginRouter;
--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -6,23 +6,21 @@ const SECRET = process.env.JWT_SECRET || 'yourSecretKey'; // Replace with your o
  * Generates a JWT token from the given payload.
  *
  * @param {object} payload - The payload to encode in the token.
- * @param {string} [expiresIn='1h'] - The expiration time of the token. e.g. `"1h"`, `"2d"`, etc.
+ * @param {jwt.SignOptions} [options] - The expiration time of the token. e.g. `"1h"`, `"2d"`, etc.
  * @returns {string} The generated JWT token.
  */
-export function generateToken(payload, expiresIn = '1h') {
-  const token = jwt.sign(payload, SECRET, {
-    expiresIn: '1h', // Token expiration time
-  });
-  return token;
+export function generateToken(payload, options = { expiresIn: '1h' }) {
+	const token = jwt.sign(payload, SECRET, options);
+	return token;
 }
 
 export function verifyToken(token) {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, SECRET, (err, payload) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(payload);
-    });
-  })
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, SECRET, (err, payload) => {
+			if (err) {
+				return reject(err);
+			}
+			resolve(payload);
+		});
+	})
 }
